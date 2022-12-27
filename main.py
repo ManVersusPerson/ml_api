12c47c5dc2ad275169576d1aaf69280a84e686ac
Akashy <jen.danilow2013@yandex.ru>
2022-12-27 01:04:54 -0800

    Added Helsinki-NLP/opus-mt-en-ru

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+class Item(BaseModel):
+        text: str
 
 
 app = FastAPI()
+classifier = pipeline("translation_en_to_ru",
+                        model = "Helsinki-NLP/opus-mt-en-ru")
 
 
 @app.get("/")
 def root():
-    return {"title":"Здесь будет наше приложение"}
+    return {"title":"Here will be our application"}
+
+@app.post("/predict/")
+def predict(item: Item):
+	return classifier(item.text)[0]
+
