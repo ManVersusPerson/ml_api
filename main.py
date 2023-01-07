64484984734b2d56f7498d65d243c6d05e6b05fb
Akashy <jen.danilow2013@yandex.ru>
2023-01-07 00:56:46 -0800

    Added Helsinki-NLP/opus-mt-en-ru and ru-en

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ class Item(BaseModel):
 
 
 app = FastAPI()
-classifier = pipeline("translation_en_to_ru",
+classifier_en_ru = pipeline("translation_en_to_ru",
                         model = "Helsinki-NLP/opus-mt-en-ru")
+
+classifier_ru_en = pipeline("translation_ru_to_en",
+                        model = "Helsinki-NLP/opus-mt-ru-en")
+
 
 
 @app.get("/")
 def root():
-    return {"title":"Here will be our application"}
+	return {"message":"Here will be our application"}
 
 @app.post("/predict/")
 def predict(item: Item):
-	return classifier(item.text)[0]
-
+	count = 0
+	dict = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+	for it in item.text:
+		if it in dict:
+			count += 1
+	if count / (len(str(item.text))) * 100 > 70:
+		return classifier_en_ru(item.text)[0]
+	else:
+		return classifier_ru_en(item.text)[0]
